6e5c15657dfde0d312acf2752f53fbaf30140fa1
Kevin O'Reilly <molldokor@gmail.com>
2018-08-07 22:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musiscians, instruments) {
  var musiciansInstruments = [];
  for (var i = 0; i < 5; i++) {
-   musiciansInstruments.unshift(`${musiscians[i]} plays ${instruments[i]}`);
+   musiciansInstruments.push(`${musiscians[i]} plays ${instruments[i]}`);
  }
  return musiciansInstruments;
 }
